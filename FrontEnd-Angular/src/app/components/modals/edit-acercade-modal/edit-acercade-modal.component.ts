7dcdc7494c581ec3bc0a035ec97c8db4406bf381
LeninQuintero <LeninQuinteroQuijano@gmail.com>
2022-11-01 13:39:10 -0300

    field validators aboutMe OK

--- FrontEnd-Angular/src/app/components/modals/edit-acercade-modal/edit-acercade-modal.component.ts
+++ FrontEnd-Angular/src/app/components/modals/edit-acercade-modal/edit-acercade-modal.component.ts
@@ -14,12 +14,31 @@ export class EditAcercadeModalComponent implements OnInit {
   constructor(private fb: FormBuilder, private userService: UserService) {
 
     this.aboutMeForm = this.fb.group({
-      aboutMe: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(400)]]
+      aboutMe: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(600)]]
     });
 
     this.userService.user.subscribe( user =>
-      this.aboutMeForm.controls['aboutMe'].setValue(user.aboutMe));
-      
+      this.aboutMeForm.controls['aboutMe'].setValue(user.aboutMe));   
+  }
+
+  isValidField(field: string) {
+    const fieldName = this.aboutMeForm.get(field);
+    return fieldName?.valid && (fieldName?.touched || fieldName?.dirty);
+  }
+
+  isInvalidField(field: string) {
+    const fieldName = this.aboutMeForm.get(field);
+    return fieldName?.invalid && (fieldName?.touched || fieldName?.dirty);
+  }
+
+  errorsFeedback(field: string, validator: string) {
+    const fieldName = this.aboutMeForm.get(field);
+    return fieldName?.hasError(validator);
+  }
+
+  requiredType(field: string, validator: string, type: string) {
+    const fieldName = this.aboutMeForm.get(field);
+    return fieldName?.errors?.[validator]?.[type];
   }
 
   ngOnInit(): void { }
