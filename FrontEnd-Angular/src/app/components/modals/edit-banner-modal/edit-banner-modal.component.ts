85e8d1fbd84f1a7f17dea1a9ccc2b0a165215457
LeninQuintero <LeninQuinteroQuijano@gmail.com>
2022-11-29 06:58:10 -0300

    banner modal changes

--- FrontEnd-Angular/src/app/components/modals/edit-banner-modal/edit-banner-modal.component.ts
+++ FrontEnd-Angular/src/app/components/modals/edit-banner-modal/edit-banner-modal.component.ts
@@ -1,18 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { UploadFilesService } from 'src/app/services/upload-files.service';
-import { Observable } from 'rxjs';
-import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-edit-banner-modal',
   templateUrl: './edit-banner-modal.component.html',
   styleUrls: ['./edit-banner-modal.component.scss']
 })
-export class EditBannerModalComponent implements OnInit {
+export class EditBannerModalComponent {
 
   constructor(private uploadFilesService: UploadFilesService) { }
 
-  ngOnInit(): void {}
+
 
 upBannerMobile(event: any) {
 const image = event.target.files[0];
