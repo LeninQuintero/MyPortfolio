d73b61924fb7494dc7be1195c97e77ef545e41c8
LeninQuintero <LeninQuinteroQuijano@gmail.com>
2022-12-17 08:12:34 -0300

    controller file spring

--- BackEnd-SpringBoot/src/main/java/com/backendspringboot/portfolio/controller/FileController.java
+++ BackEnd-SpringBoot/src/main/java/com/backendspringboot/portfolio/controller/FileController.java
@@ -5,13 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-@RestController
+@Controller
 public class FileController {
 
     @Autowired
