689d40bdf0c44b7f0d94ca5307de803c41a1ef5e
LeninQuintero <LeninQuinteroQuijano@gmail.com>
2023-01-15 20:54:09 -0300

    correction images format

--- FrontEnd-Angular/src/environments/api-urls-config.ts
+++ FrontEnd-Angular/src/environments/api-urls-config.ts
@@ -1,5 +1,4 @@
 // Url API
-export const API_URL = 'http://localhost:8080';
+export const API_URL = 'https://myportfolio-deploy-backend-springboot.onrender.com';
 
 
-// export const USERS_URL = `${API_URL}/users`;
