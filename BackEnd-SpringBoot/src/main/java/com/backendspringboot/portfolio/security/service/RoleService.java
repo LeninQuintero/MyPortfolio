a1e7443452e4d58ad926f61961eff79b5ba0a7eb
LeninQuintero <LeninQuinteroQuijano@gmail.com>
2023-03-25 14:33:10 -0300

    IRoleRepository fixed

--- BackEnd-SpringBoot/src/main/java/com/backendspringboot/portfolio/security/service/RoleService.java
+++ BackEnd-SpringBoot/src/main/java/com/backendspringboot/portfolio/security/service/RoleService.java
@@ -8,13 +8,13 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import com.backendspringboot.portfolio.security.repository.IRolRepository;
+import com.backendspringboot.portfolio.security.repository.IRoleRepository;
 
 @Service
 @Transactional
 public class RoleService {
     @Autowired
-    IRolRepository irolRepository;
+    IRoleRepository irolRepository;
     
     public Optional<Role> getbyRoleName(RoleName roleName){
         return irolRepository.findByRoleName(roleName);
